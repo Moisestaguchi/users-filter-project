8b6cdd66b76d6e0f40b98a07538d18a266c6f28b
Moisestaguchi <moisestaguchi703@gmail.com>
2026-02-23 12:23:30 -0300

    refactor(user-filter): restructure project and prepare user data handling

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from "@angular/material/button"
 import { MatIconModule } from '@angular/material/icon';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { AngularMateialModule } from "../angular-material/angular-material.module";
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        UserDetailsComponent
+    ],
     imports: [
-        MatButtonModule,
-        MatIconModule
-        
+        AngularMateialModule
     ],
     exports: [
-        MatButtonModule,
-        MatIconModule
+        UserDetailsComponent
     ],
 
 })
