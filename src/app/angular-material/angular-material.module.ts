7568ef462c8f2d1410804e6da64493c7e0bafa83
Moisestaguchi <moisestaguchi703@gmail.com>
2026-02-28 10:39:10 -0300

    feat(filter): create filter component structure and initial UI layout

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from "@angular/core";
 
 import { MatDividerModule } from "@angular/material/divider";
 import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     imports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule,
     ],
-    exports: [  
+    exports: [
         MatListModule,
-        MatDividerModule
+        MatDividerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatNativeDateModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule,
     ],
 })
-export class AngularMateialModule {}
+export class AngularMateialModule { }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from "@angular/core";
 
-import { MatButtonModule } from "@angular/material/button"
-import { MatIconModule } from '@angular/material/icon';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { AngularMateialModule } from "../angular-material/angular-material.module";
+import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations: [
-        UserDetailsComponent
+        UserDetailsComponent,
+        FilterComponent,
     ],
     imports: [
-        AngularMateialModule
+        AngularMateialModule,
+        FormsModule,
+        CommonModule,
     ],
     exports: [
-        UserDetailsComponent
+        UserDetailsComponent,
+        FilterComponent,
     ],
 
 })
